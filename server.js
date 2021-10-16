fda2c09994ad9e7230020886f41717bc88d9cc34
Ajay Kumar <ajaykan47@gmail.com>
2021-10-17 00:38:15 +0530

    source-code

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require('./routes/api/users');
+const { MongoClient } = require('mongodb');
+require('./config/passport')(passport);
+
+const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+app.listen(9001);
+
+// const db = require('./config/keys').mongoURI;
+
+// mongoose.connect(db, { useNewUrlParser: true })
+//     .then(() =>
+//         console.log('MongoDB successfully connected.')
+//     ).catch(err => console.log(err));
+
+
+const uri = "mongodb+srv://ajay:Chetu@123@cluster0.hr6ka.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.use(passport.initialize());
+
+app.use('/api', users);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
